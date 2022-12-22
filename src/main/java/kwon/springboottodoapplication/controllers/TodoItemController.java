c5283b87928c52809333fe47499c47b805760d4c
Kwon-sang <kwonsg514@gmail.com>
2022-12-22 20:56:35 +0900

    add update and delete forms

--- src/main/java/kwon/springboottodoapplication/controllers/TodoItemController.java
+++ src/main/java/kwon/springboottodoapplication/controllers/TodoItemController.java
@@ -1,12 +1,22 @@
 package kwon.springboottodoapplication.controllers;
 
+import kwon.springboottodoapplication.models.TodoItem;
 import kwon.springboottodoapplication.repositories.TodoItemRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.time.Instant;
 
 @Controller
 public class TodoItemController {
@@ -21,5 +31,27 @@ public class TodoItemController {
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("todoItems", todoItemRepository.findAll());
         return modelAndView;
+    }
+
+    @PostMapping("/todo/{id}")
+    public String update(@PathVariable("id") long id, @Validated TodoItem todoItem, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            todoItem.setId(id);
+            return "update-todo-item";
+        }
+
+        todoItem.setModifiedDate(Instant.now());
+        todoItemRepository.save(todoItem);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") long id, Model model) {
+        TodoItem todoItem = todoItemRepository
+                .findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("TodoItem id : " + id + " not found"));
+        todoItemRepository.delete(todoItem);
+        return "redirect:/";
     }
 }
